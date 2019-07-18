7442f609bd46c5770967b77c490ecfe9f678047f
NikolaevAB <nikolaev1972.72@mail.ru>
2019-07-18 18:07:16 +0300

    commit1

--- src/HomeWork5_is_Perfect.java
+++ src/HomeWork5_is_Perfect.java
@@ -1,6 +1,22 @@
 
 public class HomeWork5_is_Perfect {
     public static void main(String[]args){
+        HomeWork5_is_Perfect.isPerfect2(8128);
+        //HomeWork5_is_Perfect.isPerfect(100000);//100.000 cycles: 1min 33 sec
+    }
+
+    public static void isPerfect2(int num) {
+        String str = "not perfect.";
+        int res = 0;
+
+        for (int i = 1; i < num; i++) {
+            res = ((num % i) == 0) ? (res + i) : res;
+        }
+
+        if (num == res) str = "perfect.";
+
+        System.out.println("This number is " + str);
+
         HomeWork5_is_Perfect.isPerfect(1000000);//100.000 cycles: 1min 33 sec
     }
 
